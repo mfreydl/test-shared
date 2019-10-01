220fde23244e1efc95e679e479a2d39f19a7d17c
mfreydl <letmeaskmydog@gmail.com>
2019-10-01 07:59:48 -0400

    Added asSmallLetters(); Moved output to dist folder

---
+++ dist/utils.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/** Lowercases the given string */
+function asSmallLetters(input) {
+    return input.toLowerCase();
+}
+exports.asSmallLetters = asSmallLetters;
+//# sourceMappingURL=utils.js.map
---
+++ utils.ts
@@ -0,0 +1,5 @@
+
+/** Lowercases the given string */
+export function asSmallLetters(input: string): string {
+  return input.toLowerCase();
+}
