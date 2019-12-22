f42cfd063b84b8f66ba08f56f4bac4b8b6d6b934
mfreydl <mfreydl@users.noreply.github.com>
2019-12-21 21:12:54 -0600

    Delete utils.js

--- dist/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-/** Lowercases the given string */
-function asSmallLetters(input) {
-    return input.toLowerCase();
-}
-exports.asSmallLetters = asSmallLetters;
-//# sourceMappingURL=utils.js.map
